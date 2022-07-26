b566a3b3afed964f7885b6b9bf6e7342c4a169cb
Emmanuel Zelarayan <emmavz@live.com>
2022-07-26 20:19:49 -0300

    cloudinary config

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,11 +4,12 @@ const { StatusCodes } = require("http-status-codes");
 const createProduct = async (req, res) => {
   console.log(req.body);
   const product = await Product.create(req.body);
-  res.status(StatusCodes.CREATED).json({product});
+  res.status(StatusCodes.CREATED).json({ product });
 };
 
 const getAllProducts = async (req, res) => {
-  res.send("List of products");
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({ products });
 };
 
 module.exports = {
